4e4f9c6d7171581368eb5286b98c82e1091d5619
Lam Ngoc Khuong <me@ngockhuong.com>
2018-11-19 21:44:55 +0700

    init project

---
+++ src/main/java/com/ngockhuong/example/SpringbootRestAuth0JwtAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.ngockhuong.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestAuth0JwtAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestAuth0JwtAuthenticationApplication.class, args);
+    }
+}
